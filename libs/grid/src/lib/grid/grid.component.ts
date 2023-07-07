aaf4641bde787b32cb17f79d712c060e4862e9d8
xocomil <xocomil@gmail.com>
2023-07-06 19:32:25 -0600

    add hints to puzzle

--- libs/grid/src/lib/grid/grid.component.ts
+++ libs/grid/src/lib/grid/grid.component.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @angular-eslint/no-host-metadata-property */
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  inject,
+} from '@angular/core';
 import { GridStateService } from './grid.state.service';
 
 @Component({
@@ -11,16 +16,32 @@ import { GridStateService } from './grid.state.service';
     class: 'text-accent',
   },
   template: `
+    <div></div>
     <div
-      class="border border-primary rounded cell text-[80px]/[80px] font-bold text-center"
-      [class.bg-primary]="cell.value === 'filled'"
-      *ngFor="let cell of state.solutionCells()"
+      class="border border-primary rounded cell inline-flex justify-center items-center"
+      *ngFor="let colHint of state.columnHints()"
     >
-      <!-- <span class="text-xs"
-        >{{ cell.value }} [{{ cell.col }}, {{ cell.row }}]</span
-      > -->
-      <span *ngIf="cell.value === 'flagged'">X</span>
+      {{ colHint }}
     </div>
+    <ng-container *ngFor="let row of state.solution(); index as index">
+      <div
+        class="border border-primary rounded cell inline-flex justify-center items-center"
+      >
+        {{ state.rowHints()[index] }}
+      </div>
+
+      <div
+        class="border border-primary rounded cell text-[80px]/[80px] font-bold text-center"
+        [class.bg-primary]="cell.value === 'filled'"
+        *ngFor="let cell of row"
+      >
+        <!--      <span class="text-xs"-->
+        <!--        >{{ cell.value }} [{{ cell.col }}, {{ cell.row }}]</span-->
+        <!--      >-->
+        <span *ngIf="cell.value === 'flagged'">X</span>
+      </div>
+    </ng-container>
+    <button class="btn btn-primary" (click)="refresh()">Refresh</button>
   `,
   styleUrls: ['./grid.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,4 +49,10 @@ import { GridStateService } from './grid.state.service';
 })
 export class GridComponent {
   protected state = inject(GridStateService);
+
+  @HostBinding('style.--grid-size') gridSize = this.state.gridSize() + 1;
+
+  protected refresh() {
+    this.state.refresh();
+  }
 }
--- libs/grid/src/lib/grid/grid.state.service.ts
+++ libs/grid/src/lib/grid/grid.state.service.ts
@@ -1,18 +1,67 @@
 import { Injectable, computed, signal } from '@angular/core';
 import { Cell } from '../models/cell.model';
 
+const GRID_SIZE = 10 as const;
+
 @Injectable()
 export class GridStateService {
-  solution = signal(createSolutionGrid());
-  solutionCells = computed(() => this.solution().flat());
+  readonly gridSize = signal(GRID_SIZE);
+  readonly solution = signal(createSolutionGrid(this.gridSize()));
+
+  readonly rowHints = computed(() =>
+    this.solution().map((row) => calculateHints(row)),
+  );
+
+  readonly #columns = computed(() =>
+    this.solution().reduce(
+      (cur, row) => {
+        row.forEach((cell) => {
+          cur[cell.col][cell.row] = cell;
+        });
+
+        return cur;
+      },
+      Array.from({ length: this.solution()[0].length }, () => []) as Cell[][],
+    ),
+  );
+
+  readonly columnHints = computed(() =>
+    this.#columns().map((column) => calculateHints(column)),
+  );
+
+  refresh() {
+    this.solution.set(createSolutionGrid(this.gridSize()));
+  }
 }
 
-const createSolutionGrid = (): Cell[][] =>
-  Array.from({ length: 3 }, (_, col) => createSolutionGridRow(col));
+const createSolutionGrid = (gridSize: number): Cell[][] =>
+  Array.from({ length: gridSize }, (_, row) =>
+    createSolutionGridRow(row, gridSize),
+  );
 
-const createSolutionGridRow = (col: number): Cell[] =>
-  Array.from({ length: 3 }, (_, row) => ({
+const createSolutionGridRow = (row: number, gridSize: number): Cell[] =>
+  Array.from({ length: gridSize }, (_, col) => ({
     row,
     col,
     value: Math.floor(Math.random() * 2) === 0 ? 'filled' : 'flagged',
   }));
+
+const calculateHints = (cells: Cell[]): number[] =>
+  cells
+    .reduce(
+      (hints, curCell) => {
+        if (curCell.value === 'filled') {
+          hints[hints.length - 1]++;
+
+          return hints;
+        }
+
+        if (hints[hints.length - 1] !== 0) {
+          hints.push(0);
+        }
+
+        return hints;
+      },
+      [0] as number[],
+    )
+    .filter((value, index) => index === 0 || value > 0);
